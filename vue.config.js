1699b6e32512194fe8e3713f126d501b9dbd9776
eller <elleryba@icloud.com>
2022-03-23 10:35:23 -0400

    - take 45

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
-  publicPath: process.env.NODE_ENV === 'production' ? '/elleryba.github.io/' : '/'
+  //publicPath: process.env.NODE_ENV === 'production' ? '/elleryba.github.io/' : '/'
 }
